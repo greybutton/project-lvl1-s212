1c787e90c5fc178c6a679aefdda4c38901b55bf5
greybutton <greybutton@yandex.com>
2017-12-14 21:34:18 +0800

    add step-8
    brain-progression

---
+++ src/games/progression.js
@@ -0,0 +1,23 @@
+import gameplay from '..';
+
+const description = 'What number is missing in this progression?';
+
+const progressionLength = 10;
+const randomNumber = () => Math.floor(Math.random() * ((10 - 0) + 1)) + 0;
+
+const logic = () => {
+  const start = randomNumber();
+  const step = randomNumber();
+  const gap = randomNumber();
+  const numbers = Array(progressionLength).fill(null).map((e, i) => start + (step * i));
+  const question = numbers.map((e, i) => (i === gap ? '..' : e)).join(' ');
+  const answer = numbers[gap];
+  return {
+    question,
+    answer: `${answer}`,
+  };
+};
+
+const game = () => gameplay({ description, logic });
+
+export default game;
